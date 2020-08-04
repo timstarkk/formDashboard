f477a1d4d86df1b2cd8f6a143d9c2d025ac62a64
timstarkk <tim@timstark.dev>
2020-08-03 19:19:43 -0500

    un-comment line making page subtitle visible

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -5,7 +5,7 @@ export default function Banner({ children, title, subtitle }) {
     return (
         <div className="banner">
             <h1>{title}</h1>
-            {/* <p className="banner-subtitle">{subtitle}</p> */}
+            <p className="banner-subtitle">{subtitle}</p>
             {/* <div className="social">
                 <a href="https://www.linkedin.com/in/tim-stark-00722a113/" className="icon-button" target="_blank">
                     <FaLinkedin />
