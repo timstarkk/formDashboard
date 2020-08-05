698570b10d904d8798373a838df82e16b62f70ad
timstarkk <tim@timstark.dev>
2020-08-05 15:37:33 -0500

    added empty <p> element for whitespace

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,6 +6,7 @@ export default function Footer() {
         <section className="footer-section">
             <div></div>
             <p>Tim Stark 2020</p>
+            <p></p>
             {/* <h4>(images from Unsplash and Adidas)</h4> */}
         </section>
     )
