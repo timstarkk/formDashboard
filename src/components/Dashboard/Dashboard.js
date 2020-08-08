a3e4863b45885c4b3fe19a692bdb87ec8e4db30d
timstarkk <tim@timstark.dev>
2020-08-07 22:14:15 -0500

    renamed products to dashboard

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { ItemContext } from '../../context';
+import './Dashboard.css';
+import Item from '../Item/Item';
+import Loading from '../Loading/Loading';
+
+export default class Dashboard extends Component {
+    static contextType = ItemContext;
+    render() {
+        let { loading, shopItems: items } = this.context;
+
+        items = items.map(item => {
+            return <Item key={item.id} item={item} />
+        });
+
+        return (
+            <section className="dashboard-section">
+                <h4>Dashboard</h4>
+                <div className='line'></div>
+            </section>
+        )
+    }
+}
