b3f04486dddf8b02cd8845af6d1e3f959db4e81e
timstarkk <tim@timstark.dev>
2020-08-07 22:17:52 -0500

    removed paragraph contents for new webpage

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,9 +7,7 @@ export default function About() {
             <h4>About This Site</h4>
             <div className='line'></div> 
             <div className="container-wrapper">
-                <p>A serverless E-commerce website built with React.js and AWS Cloud.<br />AWS services used include Amplify, Cognito, AppSync, DynamoDB, and S3.<br />
-                    The shopping cart was built without libraries, and includes cart persistence during user sign-in. <br /><br />
-                    This website is an example and is not intended to actually sell products.<br />For this reason, the checkout button currently has no functionality.</p>
+                <p></p>
             </div>
         </div>
     )
