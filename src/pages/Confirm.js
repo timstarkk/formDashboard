7131b3e935015703b59f90d30d1d95a34a2570d0
timstarkk <tim@timstark.dev>
2020-08-18 00:45:29 -0500

    updated import

--- src/pages/Confirm.js
+++ src/pages/Confirm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Footer from '../components/Footer/Footer';
-import ConfirmForm from '../components/Account/ConfirmForm';
+import ConfirmForm from '../components/Authentication/ConfirmForm';
 
 export default function SignUp() {
     return (
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Footer from '../components/Footer/Footer';
-import SignInForm from '../components/Account/SignInForm';
+import SignInForm from '../components/Authentication/SignInForm';
 
 export default function SignIn() {
     return (
