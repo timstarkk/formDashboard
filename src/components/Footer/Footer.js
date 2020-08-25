a04d8554331de7469fc93e4b4bba3ad8cef4748b
timstarkk <tim@timstark.dev>
2020-08-24 20:45:46 -0500

    changed <p> contents

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     return (
         <section className="footer-section">
             <div></div>
-            <p>Tim Stark 2020</p>
+            <p>Made by Tim Stark</p>
             <p></p>
             {/* <h4>(images from Unsplash and Adidas)</h4> */}
         </section>
