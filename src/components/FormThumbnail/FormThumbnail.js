067f1549a01a1f6de17be98604cb4dbd32719727
timstarkk <tim@timstark.dev>
2020-09-10 03:01:21 -0500

    initial creation of FormThumbnail component

---
+++ src/components/FormThumbnail/FormThumbnail.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './FormThumbnail.css';
+
+export default function FormThumbnail() {
+    return (
+        <div>
+            hello from FormThumbnail
+        </div>
+    );
+};
