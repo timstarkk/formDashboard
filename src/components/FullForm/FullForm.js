4419ff2c75205fe25d524a0d51712e5df24ca54e
timstarkk <tim@timstark.dev>
2020-09-25 15:54:36 -0500

    removed whitespace

--- src/components/FullForm/FullForm.js
+++ src/components/FullForm/FullForm.js
@@ -15,28 +15,28 @@ export default class FullForm extends Component {
     };
 
     render() {
-        // let { toggleCart, cartVisible, cartItemsData } = this.context;
-        // let visibility = "hide";
-        // let subtotal = 0;
+        let { toggleCart, cartVisible, cartItemsData } = this.context;
+        let visibility = "hide";
+        let subtotal = 0;
 
-        // for (const item of cartItemsData) {
-        //     subtotal += item.price * item.amount
-        // };
+        for (const item of cartItemsData) {
+            subtotal += item.price * item.amount
+        };
 
         // can transform cartVisible into another variable
         // for opening and closing the dashboard toolbox
-        // if (cartVisible) {
-        //     visibility = 'show';
-        //     document.getElementById('app-container').classList.add('show');
-        //     document.body.classList.add('show');
-        //     document.getElementsByTagName( 'html' )[0].classList.add('show');
-        // } else {
-        //     if (document.getElementById('app-container') !== null) {
-        //         document.getElementById('app-container').classList.remove('show');
-        //         document.body.classList.remove('show');
-        //         document.getElementsByTagName( 'html' )[0].classList.remove('show');
-        //     }
-        // };
+        if (cartVisible) {
+            visibility = 'show';
+            document.getElementById('app-container').classList.add('show');
+            document.body.classList.add('show');
+            document.getElementsByTagName('html')[0].classList.add('show');
+        } else {
+            if (document.getElementById('app-container') !== null) {
+                document.getElementById('app-container').classList.remove('show');
+                document.body.classList.remove('show');
+                document.getElementsByTagName('html')[0].classList.remove('show');
+            }
+        };
         return (
             // <>
             //     <div id="menu-background" className={visibility} onClick={() => toggleCart()} />
