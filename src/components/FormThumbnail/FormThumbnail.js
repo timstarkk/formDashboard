816b233890541aabdea736bca09872a76efd5f4a
timstarkk <tim@timstark.dev>
2020-09-29 17:22:03 -0500

    import context and add handleSelectForm to onclick

--- src/components/FormThumbnail/FormThumbnail.js
+++ src/components/FormThumbnail/FormThumbnail.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './FormThumbnail.css';
 import { ItemContext } from '../../context';
+import Item from '../Item/Item';
 
 export default function FormThumbnail(form) {
-    const {id, contentsArray} = form.form;
+    const { id, contentsArray } = form.form;
+
+    const { handleSelectForm } = useContext(ItemContext)
     
     return (
         <>
             <div id="formThumbnailBox">
-                <div className="thumbnailItem" onClick={() => console.log('you clicked it')}>
+                <div className="thumbnailItem" onClick={handleSelectForm}>
                     <div>
                         id: {id}
                     </div>
