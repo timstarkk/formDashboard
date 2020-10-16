0ca63d9f63b933ee4082b46963ccae10282baab2
Tim Stark <timstarkk@icloud.com>
2020-10-16 00:15:13 -0500

    fix imports/change static

--- src/components/MyFirstGrid/MyFirstGrid.js
+++ src/components/MyFirstGrid/MyFirstGrid.js
@@ -1,10 +1,11 @@
 import GridLayout from 'react-grid-layout';
+import React, { Component } from 'react';
  
-export default class MyFirstGrid extends React.Component {
+export default class MyFirstGrid extends Component {
   render() {
     // layout is an array of objects, see the demo for more complete usage
     const layout = [
-      {i: 'a', x: 0, y: 0, w: 1, h: 2, static: true},
+      {i: 'a', x: 0, y: 0, w: 1, h: 2},
       {i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
       {i: 'c', x: 4, y: 0, w: 1, h: 2}
     ];
