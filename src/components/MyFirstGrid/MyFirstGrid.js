534b193d32ed0acf2b2d386bd4b21e390d99b21b
Tim Stark <timstarkk@icloud.com>
2020-10-16 01:46:07 -0500

    push for dev purpose - responsive grid working

--- src/components/MyFirstGrid/MyFirstGrid.js
+++ src/components/MyFirstGrid/MyFirstGrid.js
@@ -1,21 +1,33 @@
-import GridLayout from 'react-grid-layout';
+import {Responsive, WidthProvider} from 'react-grid-layout';
 import React, { Component } from 'react';
 import './MyFirstGrid.css';
+
+const ResponsiveReactGridLayout = WidthProvider(Responsive);
  
 export default class MyFirstGrid extends Component {
   render() {
     // layout is an array of objects, see the demo for more complete usage
-    const layout = [
-      {i: 'a', x: 0, y: 0, w: 1, h: 2},
-      {i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
-      {i: 'c', x: 4, y: 0, w: 1, h: 2}
-    ];
+    const layouts = {
+        lg: [
+            {i: 'a', x: 0, y: 0, w: 12, h: 4},
+            {i: 'b', x: 0, y: 1, w: 6, h: 4},
+            {i: 'c', x: 6, y: 1, w: 6, h: 4},
+            {i: 'd', x: 0, y: 2, w: 12, h: 4}
+            ]
+    };
+
     return (
-      <GridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
-        <div key="a">a</div>
-        <div key="b">b</div>
-        <div key="c">c</div>
-      </GridLayout>
+      <ResponsiveReactGridLayout 
+      className="layout" 
+      layouts={layouts}
+      breakpoints={{lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0}}
+      cols={{lg: 12, md: 12, sm: 12, xs: 12, xxs: 12}}
+      rowHeight={30}>
+        <div className="grid-item" key="a">a</div>
+        <div className="grid-item" key="b">b</div>
+        <div className="grid-item" key="c">c</div>
+        <div className="grid-item" key="d">d</div>
+      </ResponsiveReactGridLayout>
     );
   };
 };
