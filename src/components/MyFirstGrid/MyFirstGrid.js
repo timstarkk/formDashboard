96dad21ac2be78fd96e688fa4e68efbe72aa30b3
Tim Stark <timstarkk@icloud.com>
2020-10-18 01:37:18 -0500

    add constructor and onLayoutChange method

--- src/components/MyFirstGrid/MyFirstGrid.js
+++ src/components/MyFirstGrid/MyFirstGrid.js
@@ -7,13 +7,27 @@ const ResponsiveReactGridLayout = WidthProvider(Responsive);
  
 export default class MyFirstGrid extends Component {
   static contextType = ItemContext;
+  constructor(props) {
+    super();
+    this.onLayoutChange = this.onLayoutChange.bind(this);
+  }
+
+  onLayoutChange(layout, layouts) {
+    console.log(layout);
+    console.log(layouts);
+    // below copied from example
+    // this.props.onLayoutChange(layout, layouts);
+  }
+
   render() {
     const { layouts, updateLayouts } = this.context;
 
+    console.log(layouts);
     return (
       <ResponsiveReactGridLayout 
       className="layout" 
       layouts={layouts}
+      onLayoutChange={this.onLayoutChange}
       breakpoints={{lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0}}
       cols={{lg: 12, md: 12, sm: 12, xs: 12, xxs: 12}}
       rowHeight={30}>
