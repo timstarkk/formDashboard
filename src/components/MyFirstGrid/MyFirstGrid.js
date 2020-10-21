92f4928a373ef5e91da89108e54d39b0347b01ce
Tim Stark <timstarkk@icloud.com>
2020-10-21 00:10:32 -0500

    added comment/ commit for amplify build

--- src/components/MyFirstGrid/MyFirstGrid.js
+++ src/components/MyFirstGrid/MyFirstGrid.js
@@ -47,6 +47,7 @@ export default class MyFirstGrid extends Component {
       breakpoints={{lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0}}
       cols={{lg: 12, md: 12, sm: 12, xs: 12, xxs: 12}}
       rowHeight={30}>
+        {/* have these grid items populate based on database object.*/}
         <div className="grid-item" key="a">a</div>
         <div className="grid-item" key="b">b</div>
         <div className="grid-item" key="c">c</div>
