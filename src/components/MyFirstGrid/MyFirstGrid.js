d2bb517dbf2b84dda5b1dc74e06fc64a53edb61c
Tim Stark <timstarkk@icloud.com>
2020-10-21 03:04:32 -0500

    delete created method writeDivs/call displayForm

--- src/components/MyFirstGrid/MyFirstGrid.js
+++ src/components/MyFirstGrid/MyFirstGrid.js
@@ -44,22 +44,24 @@ export default class MyFirstGrid extends Component {
   }
 
   render() {
-    const { layouts } = this.context;
-    console.log(this.state.layouts);
+    const { layouts, displayForm } = this.context;
     return (
-      <ResponsiveReactGridLayout 
-      className="layout" 
-      layouts={layouts}
-      onLayoutChange={this.onLayoutChange}
-      breakpoints={{lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0}}
-      cols={{lg: 12, md: 12, sm: 12, xs: 12, xxs: 12}}
-      rowHeight={30}>
-        {/* have these grid items populate based on database object.*/}
-        <div className="grid-item" key="a">a</div>
-        <div className="grid-item" key="b">b</div>
-        <div className="grid-item" key="c">c</div>
-        <div className="grid-item" key="d">d</div>
-      </ResponsiveReactGridLayout>
+      <>
+        {displayForm()}
+        <ResponsiveReactGridLayout 
+        className="layout" 
+        layouts={layouts}
+        onLayoutChange={this.onLayoutChange}
+        breakpoints={{lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0}}
+        cols={{lg: 12, md: 12, sm: 12, xs: 12, xxs: 12}}
+        rowHeight={30}>
+          {/* have these grid items populate based on database object.*/}
+          <div className="grid-item" key="a">a</div>
+          <div className="grid-item" key="b">b</div>
+          <div className="grid-item" key="c">c</div>
+          <div className="grid-item" key="d">d</div>
+        </ResponsiveReactGridLayout>
+      </>
     );
   };
 };
