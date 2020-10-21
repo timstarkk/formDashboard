956eee5b42ffb13c5478ee59a33c6dbcd70bddd8
Tim Stark <timstarkk@icloud.com>
2020-10-20 23:19:59 -0500

    comment out react-grid css to test

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ import Navbar from './components/Navbar/Navbar';
 import AboutPage from './pages/AboutPage';
 import ConfirmPage from './pages/Confirm';
 
-import '../node_modules/react-grid-layout/css/styles.css';
-import '../node_modules/react-resizable/css/styles.css';
+// import '../node_modules/react-grid-layout/css/styles.css';
+// import '../node_modules/react-resizable/css/styles.css';
 
 Amplify.configure(config);
 function App() {
