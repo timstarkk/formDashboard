bdaf9dee62b0ee6ad3001c9b358961b7889e6cc3
Tim Stark <timstarkk@icloud.com>
2020-10-21 00:24:22 -0500

    attempt to import css for fix

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Dashboard from '../components/Dashboard/Dashboard';
+import '../components/FormThumbnail/FormThumbnail.css';
 import Footer from '../components/Footer/Footer';
 
 export default function DashboardPage() {
