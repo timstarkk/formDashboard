d257768eb524431aa40667ca8b52592e741021fc
Tim Stark <timstarkk@icloud.com>
2020-10-31 00:14:56 -0500

    create initial TextBoxProperties component

---
+++ src/components/TextBoxProperties/TextBoxProperties.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TextBoxProperties extends Component {
+    render() {
+        return (
+            <div>
+                hello from TextBoxProperties
+            </div>
+        )
+    }
+}
