a72f88975b4940bc84e98b061f70e4f70f378fec
Tim Stark <timstarkk@icloud.com>
2020-10-31 01:35:28 -0500

    initial creation of Properties component

---
+++ src/components/Properties/Properties.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Properties extends Component {
+    render() {
+        return (
+            <>
+                hello from Properties
+            </>
+        )
+    }
+}
