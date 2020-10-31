0355e275aad970c0ac619cc70eeb827c7f81fa1b
Tim Stark <timstarkk@icloud.com>
2020-10-31 02:45:00 -0500

    import TextBoxProperties

--- src/components/Properties/Properties.js
+++ src/components/Properties/Properties.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { IoIosReturnRight } from 'react-icons/io';
 import { ItemContext } from '../../context';
+import TextBoxProperties from './../TextBoxProperties/TextBoxProperties';
+
+
+
 export default class Properties extends Component {
 
     constructor(props) {
