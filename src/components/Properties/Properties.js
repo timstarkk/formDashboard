36a70c2908b7c6ae8d97a4e6d50502e0b5f2a22d
Tim Stark <timstarkk@icloud.com>
2020-10-31 01:38:51 -0500

    paste copied if then statements from context

--- src/components/Properties/Properties.js
+++ src/components/Properties/Properties.js
@@ -2,10 +2,30 @@ import React, { Component } from 'react'
 
 export default class Properties extends Component {
     render() {
-        return (
-            <>
-                hello from Properties
-            </>
-        )
+        {if (selectedType === 'text') {
+            return (
+                <>
+                    <p>text properties:</p>
+                </>
+            )
+        } else if (selectedType === 'radio') {
+            return (
+                <>
+                    <p>radio properties:</p>
+                </>
+            )
+        } else if (selectedType === 'checkbox') {
+            return (
+                <>
+                    <p>checkbox properties:</p>
+                </>
+            )
+        } else if (selectedType === 'textbox') {
+            return (
+                <>
+                    <h3>text item properties:</h3>
+                </>
+            )
+        };}
     }
 }
