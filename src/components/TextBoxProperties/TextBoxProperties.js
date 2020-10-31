6242f629e608783e262df2de74f0891bf070c6f2
Tim Stark <timstarkk@icloud.com>
2020-10-31 00:15:54 -0500

    import CSS

--- src/components/TextBoxProperties/TextBoxProperties.js
+++ src/components/TextBoxProperties/TextBoxProperties.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './TextBoxProperties.css';
 
 export default class TextBoxProperties extends Component {
     render() {
