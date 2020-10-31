e2b93023b0b5a6cb262b8adc115be0569823c6bd
Tim Stark <timstarkk@icloud.com>
2020-10-31 00:23:17 -0500

    import TextBoxProperties component

--- src/context.js
+++ src/context.js
@@ -5,6 +5,7 @@ import config from './aws-exports';
 import { getDefaultNormalizer } from '@testing-library/react';
 import { v4 as uuidv4 } from 'uuid';
 import { FiSettings as SettingsIcon, FiXCircle as XIcon } from "react-icons/fi";
+import TextBoxProperties from './components/TextBoxProperties/TextBoxProperties';
 
 Amplify.configure(config);
 const ItemContext = React.createContext();
