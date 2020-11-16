1b88823d9756ca09187d6c61d0cf7e6c8281382f
timstarkk <tim@timstark.dev>
2020-11-16 00:56:08 -0600

    import DeployForm component

--- src/pages/ShowForm.js
+++ src/pages/ShowForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Footer from '../components/Footer/Footer';
+import DeployForm from '../components/DeployForm/DeployForm';
 
 export default function SignIn() {
     return (
