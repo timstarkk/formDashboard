03b0d18b4d5a5087b54e71c0be517496f8a21e8c
timstarkk <tim@timstark.dev>
2020-11-16 01:29:27 -0600

    create development conditional render

--- src/components/DeployForm/DeployForm.js
+++ src/components/DeployForm/DeployForm.js
@@ -3,10 +3,27 @@ import './DeployForm.css';
 
 export default function DeployForm(props) {
     let formId = props.formId;
-    
-    return (
-        <div>
-            hello from DeployForm component
-        </div>
-    )
+
+    if (formId == 'false') {
+        return (
+            <div>
+                hello from false false
+            </div>
+        )
+    } else if (formId == 'true') {
+        return (
+            <div>
+                hello from true true true
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                hello from DeployForm component
+            </div>
+        )
+    }
 }
+
+
+// might need ReactDOM.render();
