c1d9408fc344968a3edef5adf057a36ab1c80ee1
timstarkk <tim@timstark.dev>
2020-11-16 01:11:45 -0600

    pull formId from props

--- src/components/DeployForm/DeployForm.js
+++ src/components/DeployForm/DeployForm.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './DeployForm.css';
 
 export default function DeployForm(props) {
+    let formId = props.formId;
+    
     return (
         <div>
             hello from DeployForm component
