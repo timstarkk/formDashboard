76b8389cc4d4642231ca8158b16ae8416735ffcb
timstarkk <tim@timstark.dev>
2020-11-16 00:51:55 -0600

    create initial DeployForm component

---
+++ src/components/DeployForm/DeployForm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './DeployForm.css';
+
+export default function DeployForm(props) {
+    return (
+        <div>
+            hello from DeployForm component
+        </div>
+    )
+}
