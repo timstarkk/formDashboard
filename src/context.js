be1360cb170d6cd046583f5bb2a39371d519c5b8
timstarkk <tim@timstark.dev>
2020-11-19 06:03:13 -0600

    update addform to fix error on userId

--- src/context.js
+++ src/context.js
@@ -150,7 +150,7 @@ class ItemProvider extends Component {
             contents: {
                 columns: 9,
                 rows: 9,
-                userId,
+                userId: `${userId}`,
                 layout: [
                     {i: 'a', x: 0, y: 0, w: 12, h: 4, type: "text", isLabel: false},
                     {i: 'b', x: 0, y: 1, w: 6, h: 4, type: "radio", isLabel: false},
@@ -171,7 +171,7 @@ class ItemProvider extends Component {
                     id: "${userId}",
                     forms: ${unquotedItems}
                 }) {
-                    id forms { id, contents { columns, rows, layout { h, i, moved, static, w, x, y, type, isLabel } } }
+                    id forms { id, contents, userId { columns, rows, layout { h, i, moved, static, w, x, y, type, isLabel } } }
                 }
             }
         `
