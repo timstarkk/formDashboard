8dcf4b46c223b0e91f1c48987c259d645304f200
timstarkk <tim@timstark.dev>
2020-11-23 01:56:48 -0600

    push to test showform render

--- src/pages/ShowForm.js
+++ src/pages/ShowForm.js
@@ -2,12 +2,12 @@ import React from 'react';
 import Footer from '../components/Footer/Footer';
 import DeployForm from '../components/DeployForm/DeployForm';
 
-export default function ShowForm(props) {
-    let formId = props.match.params.slug;
+export default function ShowForm() {
 
     return (
         <>
             {/* <DeployForm formId={formId}/> */}
+            <p>hello from showform</p>
             <Footer />
         </>
     )
