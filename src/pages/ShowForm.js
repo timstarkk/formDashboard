d7cfe71e00e9f0e7faa7f8dc8db9aba3509189cc
timstarkk <tim@timstark.dev>
2020-11-23 01:01:46 -0600

    rename SignIn to ShowForm to fix error

--- src/pages/ShowForm.js
+++ src/pages/ShowForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Footer from '../components/Footer/Footer';
 import DeployForm from '../components/DeployForm/DeployForm';
 
-export default function SignIn(props) {
+export default function ShowForm(props) {
     let formId = props.match.params.slug;
 
     return (
