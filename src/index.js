ff9df6d1218b5d3249f94130027dcb76bca0c6d5
timstarkk <tim@timstark.dev>
2020-11-30 04:42:17 -0600

    import browserHistory

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { browserHistory } from 'react-router';
 import { ItemProvider } from './context';
 import Toolbox from './components/Toolbox/Toolbox';
 
