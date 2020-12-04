7bbf1fb01bf574d110f89c76fab0d89c8083ce02
timstarkk <tim@timstark.dev>
2020-12-03 23:40:36 -0600

    run handleDeployForm and grab form using formId

--- src/components/DeployForm/DeployForm.js
+++ src/components/DeployForm/DeployForm.js
@@ -5,7 +5,7 @@ import './DeployForm.css';
 
 export default function DeployForm(props) {
     let formId = props.formId;
-    // const { handleDeployForm }  = useContext(ItemContext);
+    const { handleDeployForm }  = useContext(ItemContext);
 
     const findInDatabase = async () => {
         // await return of database query
@@ -14,60 +14,8 @@ export default function DeployForm(props) {
         // while this functjion is running.
         // need to display a loading icon during that time.
 
-        // const getForms = `
-        // query {
-        //     getUser(id: "${userId}") {
-        //         forms {
-        //             id
-        //             contents {
-        //                 columns
-        //                 rows
-        //                 layout {
-        //                     h
-        //                     i
-        //                     isBounded
-        //                     isDraggable
-        //                     isResizable
-        //                     maxH
-        //                     maxW
-        //                     minH
-        //                     minW
-        //                     moved
-        //                     resizeHandles
-        //                     static
-        //                     w
-        //                     x
-        //                     y
-        //                     type
-        //                     isLabel
-        //                     labelFor
-        //                     textValue
-        //                 }
-        //             }
-        //         }
-        //     }
-        // }
-        // `
-
-        // as you can see, the above query requires a ${userId};
-        // since this component will render on random visitors browsers,
-        // there will be no logged in user to pull userId from.
-        //
-        // for this reason, i need all forms to live top level in dynamoDB
-        // this way i can query any form directly with the formId.
-
-        // This change might require that i rewrite the query and mutations
-        // that live in context. (in context, instead of using userId to find the form,
-        // I can use userId to verify that the current logged in user is the owner of /// the form before allowing the mutations.)
-        // will need to store the userId into each form under variable 'owner' like so:
-        // owner: userId
-
-
-
-        // form might need to be its own
-        // let forms = await handleDeployForm();
-        // console.log(forms)
-        console.log('this is tim')
+        let form = await handleDeployForm(formId);
+        console.log(form)
     }
 
     if (formId == 'false') {
