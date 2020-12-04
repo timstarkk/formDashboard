c3f91d2a2bee71bf4eeeb9cd51a98660d630a068
timstarkk <tim@timstark.dev>
2020-12-04 02:12:05 -0600

    add func compoonent for conditional render

--- src/index.js
+++ src/index.js
@@ -3,20 +3,41 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ItemProvider } from './context';
 import Toolbox from './components/Toolbox/Toolbox';
 import ShowForm from './pages/ShowForm';
+import { render } from '@testing-library/react';
+
+export default function Index() {
+    
+    let path = window.location.pathname
+    let isShowform = path.toLowerCase().includes("showform")
+    
+    return (
+        isShowform ? (
+            <ItemProvider>
+                <Router>
+                    <Switch>
+                        <Route exact path="/showform/:slug" component={ShowForm} />
+                    </Switch>
+                </Router>
+            </ItemProvider>
+        ) : (
+            <ItemProvider>
+                <Router>
+                    <Toolbox />
+                    <div id="app-container">
+                        <App />
+                    </div>
+                </Router>
+            </ItemProvider>
+        )
+    )
+}
 
 ReactDOM.render(
-    <ItemProvider>
-        <Router>
-            <Toolbox />
-            <div id="app-container">
-                <App />
-            </div>
-        </Router>
-    </ItemProvider>, document.getElementById('root')
+    <Index />, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
