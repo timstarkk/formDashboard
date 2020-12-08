53431c3856c74ea881641a78a209caf304cece14
timstarkk <tim@timstark.dev>
2020-12-08 05:22:13 -0600

    import/change some items from textlabelproperties

--- src/components/TextBoxProperties/TextBoxProperties.js
+++ src/components/TextBoxProperties/TextBoxProperties.js
@@ -7,17 +7,56 @@ export default class TextBoxProperties extends Component {
     constructor(props) {
         super();
         this.state = {
+            value: '',
+            updateTextBoxPlaceholder: function() {}
         }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    // componentDidMount() {
-    // };
+    componentDidMount() {
+        const { updateTextBoxPlaceholder } = this.context;
+
+        this.setState({
+            updateTextBoxPlaceholder
+        })
+    };
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    };
+
+    handleSubmit(event) {
+        event.preventDefault();
+        const {updateTextBoxPlaceholder} = this.context;
+
+        this.state.updateTextBoxPlaceholder(this.state.value);
+    };
+
 
     render() {
         return (
             <>
                 <form onSubmit={this.handleSubmit}>
                     <p>hello from textbox properties</p>
+                    
+                    {/* placeholder */}
+                    <label>
+                        placeholder:
+                        <input type="text" name="name" placeholder="enter placeholder text: " value={this.state.value} onChange={this.handleChange} />
+                    </label>
+                    <input type="submit" value="Submit" />
+                    {/* height */}
+                    {/* width */}
+                    {/* defaultValue */}
+                    {/* borderWidth */}
+                    {/* borderColor */}
+                    {/* borderRadius */}
+                    {/* textboxColor */}
+                    {/* textColor */}
+                    {/* fontSize */}
+                    {/* fontFamily */}
                 </form>
             </>
         )
