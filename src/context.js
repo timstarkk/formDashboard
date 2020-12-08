40c6de89c2fdb513170a5b376996b34628319306
timstarkk <tim@timstark.dev>
2020-12-08 05:47:24 -0600

    create updateTextBoxWidth method

--- src/context.js
+++ src/context.js
@@ -847,6 +847,28 @@ class ItemProvider extends Component {
         }, () => {console.log('state changed');this.updateLayouts(layout)});
     };
 
+    updateTextBoxWidth = (value) => {
+        let properties = this.state.properties;
+        let layout = this.state.layouts.lg;
+        // need selected item.
+        let selectedGridItem = this.state.selectedGridItem;
+
+        // loop through layouts until i.i === selectedGridItem;
+        for (const [index, i] of layout.entries()) {
+            if (i.i === selectedGridItem) {
+                i.width = value;
+                properties[index].width = value;
+            };
+        };
+
+        this.setState({
+            layouts: {
+                lg: layout
+            },
+            properties
+        }, () => {console.log('state changed');this.updateLayouts(layout)});
+    };
+
     handleFormSubmit = (event) => {
         let formContents = [];
         let layout = this.state.layouts.lg;
